74fd585fa6a867463d2f3a4e814c4974be450497
Daniel Meneses <danielmeneses26@gmail.com>
2020-05-22 20:52:48 -0400

    Entrar

---
+++ Inicio de sesion.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--====== Title ======-->
+    <title>Inicio de Sesión T.E.H.E</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+    <h1>Inicio de sesión</h1>
+    <form action="" method="POST" name="formlogin" id="formlogin">
+<img src="" alt="Imagen Login" width="100" height="100"> <br>
+
+
+<div class="form-group mt-3 col-8"><label class="form-check-label">Usuario</label><br>
+<input type="text" id="nombre1" name="nombre1" value="" placeholder="Ingrese nombre de usuario" class="form-control" required></div><br>
+<div class="form-group  mt-3 col-8"><label class="form-check-label" >Contraseña</label><br>
+<input type="password" id="pass1" name="pass1" value="" class="form-control" placeholder="Ingrese su contraseña" required></div>
+<br>
+<input type="submit" class="btn btn-outline-success btn-lg btn-block col-8  mb-2" value="Iniciar Sesión" name="btl" id="btl">
+<br>
+
+
+</form>
+
+    <!--====== Parte Final ======-->
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Ajax Contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+
+    <!--====== Isotope js ======-->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+
+    <!--====== Scrolling Nav js ======-->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
